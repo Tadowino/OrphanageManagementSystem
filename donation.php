2765affd0fd427f3ccc34c7c61a937b1580fba55
Tadowino <tadowino254@gmail.com>
2024-04-02 12:45:21 +0300

    orphanage system

---
+++ donation.php
@@ -0,0 +1,46 @@
+<?php include './components/header.php'; ?>
+
+    <div class="ui container">
+
+        <!-- Top Navigation Bar -->
+        <?php include './components/top-menu.php'; ?>
+
+        <!-- BODY Content -->
+        <div class="ui grid">
+            <!-- Left menu -->
+            <?php include './components/side-menu.php'; ?>
+            
+            <!-- right content -->
+            <div class="twelve wide column">
+                <h1>Donations</h1>
+                <p><strong>Orphan Care & Development Foundation</strong> relies on everyone's commitment to the humanitarian cause and it's underlying values. The strongest possible support from individuals, companies and foundations is essential if we are to meet the challenges we are currently facing. Your support to is more than just a donation - it is a true act of humanity.</p>
+                <p><strong>Let's join</strong> hands and support more and more needy children - the future of any nation and help them come out of their ignorance and other problems which it entails by giving them a gift of education and providing them with all-round support essential for their overall well-being. Your donations are a vital source of income that will allow us to make significant positive impact on the society around us.</p>
+
+                <p><strong>Cash donations</strong> can be made either personally or by sending an account payee Cheque / Draft through mail.</p>
+                <p>Info for making DD/Cheque: In favour of - <strong>Orphan Care & Development Foundation</strong> payable at:***********</p>
+                <p>Orphan Care & Development Foundation</p>
+                
+                <p><a class="fluid ui button" href="donation-form.php">Click here to make your contribution</a></p>
+                
+
+                <strong>Guideline for making contribution:</strong>
+                <ul>
+                    <li>Ksh.200/- a month, can give a child Nutrition at learning centre.</li>
+                    <li>Ksh.300/- a month, can give a child Nutrition & After-school support.</li>
+                    <li>Ksh.400/- a month, can give a child complete package of Nutritio, health check-up, after-school, celebration of festivals etc.</li>
+                    <li>Ksh.1000/- a month, can be used for Sensitization & Awareness Programs including Media campaigns, workshops, seminars and capacity building initiatives.</li>
+                    <li>Ksh.5000/- a month, can be used for Infrastruture set up.</li>
+                    <li>Ksh.50000/- a month, can be used for Corpus Fund.</li>
+                </ul>
+
+                <p><strong>We respect the intention of our donors and use the money donated by them for the purpose intended by the donor.</strong></p>
+
+
+
+                <span class="p-20"></span>
+            </div>
+        </div>
+
+    </div>
+    
+<?php include './components/footer.php'; ?>
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include './components/header.php'; ?>
+
+    <div class="ui container">
+
+        <!-- Top Navigation Bar -->
+        <?php include './components/top-menu.php'; ?>
+
+        <!-- BODY Content -->
+        <div class="ui grid">
+            <!-- Left menu -->
+            <?php include './components/side-menu.php'; ?>
+            
+            <!-- right content -->
+            <div class="twelve wide column">
+                <h1>HOME</h1>
+                <h3></h3>
+                <p><strong></strong></p>
+                <p><strong></strong></p>
+                <p><strong></strong></p>                
+                <p><strong></strong></p>
+
+                <div class="ui medium center images" style="text-align: center;">
+                    <img style="width: 900px; height: 500px;" class="ui medium rounded image" src="img\Nakuru-image.webp">
+        
+                </div>
+
+                <span class="p-20"></span>
+            </div>
+        </div>
+
+    </div>
+    
+<?php include './components/footer.php'; ?>
